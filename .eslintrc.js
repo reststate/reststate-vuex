62b046e8caca904994ca195c75e692c3c8f7a068
Josh Justice <jjustice@bignerdranch.com>
2018-06-15 16:39:04 -0400

    Set up testing infrastructure

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+  "extends": "codingitwrong",
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module",
+  },
+};
