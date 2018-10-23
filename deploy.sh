c25a262949386307da6866be818d32c4df53df13
Josh Justice <me@codingitwrong.com>
2018-10-23 07:03:42 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:reststate/reststate-vuex-docs.git master
+
+cd -
