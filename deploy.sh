b6e5db2249b2aa8a5017b2f98164185b5e6f56b5
Josh Justice <me@codingitwrong.com>
2018-10-23 20:42:24 -0400

    Fix domain

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ yarn docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'https://vuex.reststate.org' > CNAME
 
 git init
 git add -A
