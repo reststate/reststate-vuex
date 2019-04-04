9d32532d8244b8075bd1af4d652509e7925198e6
Lucas Arnström <lucas@arnstrom.se>
2019-04-04 13:11:53 +0200

    initial state should not be a function, but a function call

--- src/vuex-jsonapi.js
+++ src/vuex-jsonapi.js
@@ -41,7 +41,7 @@ const resourceModule = ({ name: resourceName, httpClient }) => {
   return {
     namespaced: true,
 
-    state: initialState,
+    state: initialState(),
 
     mutations: {
       REPLACE_ALL_RECORDS: (state, records) => {
