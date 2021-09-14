082e91ecaea9c832ccdf371af7043e0e87df9915
Steve Schwarz <steve@agilitynerd.com>
2021-09-14 10:02:40 -0500

    SAM-2373 update example dependencies (#23)
    * Update example to latest Vue and reduce dependencies
    * Remove unused cypress tests from example
    * Always get latest axios

--- example/src/store.js
+++ example/src/store.js
@@ -1,10 +1,11 @@
+import MockAdapter from 'axios-mock-adapter';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
-import { resourceModule } from '@reststate/vuex';
+import { resourceModule } from '@lucro/vuex';
 
 const httpClient = axios.create({
-  baseURL: 'https://jsonapi-sandbox.herokuapp.com/',
+  baseURL: 'https://localhost:8081/',
   headers: {
     'Content-Type': 'application/vnd.api+json',
   },
@@ -16,4 +17,30 @@ export default new Vuex.Store({
   modules: {
     widgets: resourceModule({ name: 'widgets', httpClient }),
   },
+});
+
+// To avoid having to run a separate server
+// mock out the one reply we need for this example.
+const mock = new MockAdapter(httpClient);
+
+// Mock any GET request to /widgets
+// arguments for reply are (status, data, headers)
+mock.onGet('/widgets?').reply(200, {
+  data:
+    [
+      {
+        type: 'widget',
+        id: '1',
+        attributes: {
+          name: 'First Widget',
+        },
+      },
+      {
+        type: 'widget',
+        id: '2',
+        attributes: {
+          name: 'Second Widget',
+        },
+      },
+    ],
 });
